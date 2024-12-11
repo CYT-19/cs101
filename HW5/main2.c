75adf971b786af6f6cf923e1226d7c9afeeea2f6
CYT-19 <xks733004@gmail.com>
2024-12-11 21:29:08 +0800

    Update main2.c

--- HW5/main2.c
+++ HW5/main2.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
 
 int main() {
-    double PI = 4.0f;
-    int x = 1, n = -1;
+    double PI = 4.0;
+    int x = 1;
+    int n = -1;
     
-    for (int i = 3; ; i += 2, x++) {
+    for (int i = 3; ; i += 2, x++)
+    {
         PI += n *(4.0 / i);
         n = -n;
         
-        int a = PI * 1000000;
-        
-        if (a == 3141590) {
-          break;  
+        if (PI >= 3.141585 && PI < 3.141595)
+        {
+            break; 
         }
     }
+    
     printf("PI = %.5f\n", PI);
     printf("最小的x = %d\n", x+1);
+    
     return 0;
 }
