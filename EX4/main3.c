39f510d1887a7f0061fb07ef7411ee02d030f9a3
CYT-19 <xks733004@gmail.com>
2024-12-12 14:37:36 +0800

    Create main3.c

---
+++ EX4/main3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    printf(" "); 
+  for( int i = 0; i<81 ;i++)  
+    {
+        printf("%d",i/9 + 1);
+        printf("*");
+        printf("%d",i%9 + 1);
+        printf("=");
+        printf("%d",(i/9+1)*(i%9+1));
+        if(i%9 + 1 == 9)
+        {
+            printf("\n");
+        }
+        printf(" "); 
+    }
+}
